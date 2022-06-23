25495ed2b2f82af11a7631201a15d0882501a19d
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-23 20:20:41 -0300

    voltando para build jar

--- backend/src/main/java/br/com/alura/forum/ForumApplication.java
+++ backend/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -13,15 +13,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @SpringBootApplication
 @EnableSwagger2
-public class ForumApplication extends SpringBootServletInitializer{
+public class ForumApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {		
-		return builder.sources(ForumApplication.class);
-	}
+
 
 }
